9dde065383356c24cf8e3d54caf1e214f4fca63b
wuyong <biggodw@gamil.com>
2026-06-11 14:59:28 +0800

    init;

---
+++ app/pages/adduser.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        {{ user }}
+        <input type="text" v-model="user.name">
+        <input type="text" v-model="user.email">
+        <button @click="adduser">add</button>
+    </div>
+</template>
+<script setup>
+const user = ref({
+    name:'',
+    email:""
+})
+const adduser = ()=>{
+    $fetch('/api/users',{
+        method:'POST',
+        body:user.value
+    })
+}
+</script>
---
+++ app/pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+    <div>
+        <h1>nuxt prisma</h1>
+        <NuxtLink to="/user">用户</NuxtLink>
+        <NuxtLink to="/adduser">添加</NuxtLink>
+    </div>
+</template>
